e10a5b1a03017a4aa71b1567415a18b3e1d430ea
Lorenzo Souza <lorenzo.souza7@gmail.com>
2021-09-10 18:56:24 -0300

    New project

---
+++ MEMORY GAME/MEMORY GAME.js
@@ -0,0 +1,14 @@
+const FRONT = "card-front"
+const BACK = "card-back"
+
+let apps = ['bootstrap',
+    'AWS',
+    'BOOTSTRAP',
+    'CSS',
+    'GIT',
+    'HTML',
+    'JAVASCRIPT',
+    'MYSQL',
+    'POSTGRESQL',
+    'PYTHON',
+]
