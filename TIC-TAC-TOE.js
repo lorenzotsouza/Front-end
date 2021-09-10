43149e50fa4b38cc88b26abdec0b8b669407cbd0
Lorenzo Souza <lorenzo.souza7@gmail.com>
2021-09-10 14:33:05 -0300

    Corrections

--- TIC-TAC-TOE.js
+++ TIC-TAC-TOE.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-app.set("views", path.join(__dirname, 'GAME'));
+app.set("views", path.join(__dirname, '/TIC-TAC-TOE'));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
