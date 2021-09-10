e45a5e57344f20e22ce023863a27f1815aef0b4b
Lorenzo Souza <lorenzo.souza7@gmail.com>
2021-09-10 16:56:40 -0300

    Project complete

--- TIC-TAC-TOE/GAME.js
+++ TIC-TAC-TOE/GAME.js
@@ -1,15 +1,61 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
+let board = ['', '', '', '', '', '', '', '', ''];
+let playerTime = 0;
+let gameOver = false;
 
-app.set("views", path.join(__dirname , ""));
-app.set("view engine", "ejs");
+let symbols = ['o', 'x'];
 
-app.get("/", (req, res) => {
-  res.render('GAME');
-});
+let winStates = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-app.listen(7000, () => {
-  console.log('Server Running on 7000');
-});
+function handleMove(position) {
+
+  if (gameOver) {
+
+    return;
+  }
+
+  if (board[position] == '') {
+
+      board[position] = symbols[playerTime];
+
+    gameOver = isWin();
+
+  if(gameOver == false) {
+
+        playerTime = (playerTime == 0) ? 1 : 0;
+
+     }
+  }   
+
+    return gameOver;
+
+}
+
+function isWin() {
+
+  for (let i = 0; i < winStates.length; i++) {
+       let seq = winStates[i];
+
+       let pos1 = seq[0];
+       let pos2 = seq[1];
+       let pos3 = seq[2];
+
+  if (board[pos1] == board[pos2] &&
+        board[pos1] == board[pos3] &&
+        board[pos1] != '') {
+       return true;
+
+    }
+  }
+
+  return false;
+
+}
---
+++ TIC-TAC-TOE/SERVER.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.set("views", path.join(__dirname , ""));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render('GAME');
+});
+
+app.listen(7000, () => {
+  console.log('Server Running on 7000');
+});
